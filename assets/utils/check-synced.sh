b1f0063b3d36c9fb23e2003b1b6c7ea99239ef8e
Dread <dread@pop-os.localdomain>
2022-04-14 16:43:41 -0400

    updated health checks

--- assets/utils/check-synced.sh
+++ assets/utils/check-synced.sh
@@ -13,13 +13,16 @@ else
     b_password=$(yq e '.bitcoind.password' /root/start9/config.yaml)
 fi
 TXINDEX_CHECK=$(curl -sS --user $b_username:$b_password --data-binary '{"jsonrpc": "1.0", "id": "sync-hck", "method": "getindexinfo", "params": ["txindex"]}' -H 'content-type: text/plain;' $b_host:8332 | sed -n 's/.*\("synced":true\).*/1/p') 
-
+TXINDEX_SYNC=$(curl -sS --user mempool:mempool --data-binary '{"jsonrpc": "1.0", "id": "sync-hck", "method": "getindexinfo", "params": ["txindex"]}' -H 'content-type: text/plain;' btc-rpc-proxy.embassy:8332 | sed -n 's/.*\("synced":false\).*/1/p')
 check_sync(){
     DURATION=$(</dev/stdin)
     if (($DURATION <= 5000 )); then
         exit 60
+    elif test "$TXINDEX_SYNC" != 1; then
+        echo "Transaction Indexer is still syncing. Mempool will not display correctly until sync is complete." >&2
+        exit 61
     elif test "$TXINDEX_CHECK" != 1; then
-        echo "Transaction Indexer is either not enabled or has not yet synced. Mempool will not display completely." >&2
+        echo "Transaction Indexer is either not enabled. Mempool will not display completely." >&2
         exit 1
     fi
 }
@@ -34,3 +37,6 @@ case "$1" in
         echo "Commands:"
         echo "         sync"
 esac
+
+# Below is an example of the curl command being used to pull the txindex information from bitcoind. 
+## curl -sS --user mempool:mempool --data-binary '{"jsonrpc": "1.0", "id": "sync-hck", "method": "getindexinfo", "params": ["txindex"]}' -H 'content-type: text/plain;' btc-rpc-proxy.embassy:8332 | sed -n 's/.*\("synced":false\).*/2/p'
