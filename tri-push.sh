b85db33403a8f33175759a43d68bb0c60c875a53
Dread <34528298+cryptodread@users.noreply.github.com>
2022-04-14 16:29:24 -0400

    Delete tri-push.sh

--- tri-push.sh
+++
@@ -1,1 +0,0 @@
-scp *.s9pk root@embassy-ac87f163.local:/embassy-data/package-data/ && scp *.s9pk root@embassy-3f18af5e.local:/embassy-data/package-data/ && scp *.s9pk root@embassy-31485559.local:/embassy-data/package-data/
