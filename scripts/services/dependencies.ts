90805d6917ed857b3f8361b9e0bb790729308e20
Aiden McClelland <me@drbonez.dev>
2022-06-30 10:49:31 -0600

    switch to sdk lib

--- scripts/services/dependencies.ts
+++ scripts/services/dependencies.ts
@@ -1,17 +1,17 @@
-import { ExpectedExports, Config, matches } from "../deps.ts";
+import { matches, types as T } from "../deps.ts";
 
 const { shape, arrayOf, string, boolean } = matches;
 
 const matchBitcoindConfig = shape({
-    advanced: shape({
-        pruning: shape({
-        mode: string,
-        }),
+  advanced: shape({
+    pruning: shape({
+      mode: string,
     }),
-    rpc: shape({
-        enable: boolean,
-    }),
-    txindex: boolean,
+  }),
+  rpc: shape({
+    enable: boolean,
+  }),
+  txindex: boolean,
 });
 
 const matchProxyConfig = shape({
@@ -22,9 +22,10 @@ const matchProxyConfig = shape({
         "allowed-calls": arrayOf(string),
         password: string,
       },
-    )
+    ),
   ),
 });
+type ProxyChecks = typeof matchProxyConfig._TYPE;
 
 function times<T>(fn: (i: number) => T, amount: number): T[] {
   const answer = new Array(amount);
@@ -39,10 +40,11 @@ function randomItemString(input: string) {
 }
 
 const serviceName = "mempool";
-const fullChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const fullChars =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 type Check = {
-  currentError(config: Config): string | void;
-  fix(config: Config): void;
+  currentError(config: T.Config): string | void;
+  fix(config: T.Config): T.Config;
 };
 
 const proxyChecks: Array<Check> = [
@@ -57,11 +59,15 @@ const proxyChecks: Array<Check> = [
       return `Must have an RPC user named "${serviceName}"`;
     },
     fix(config) {
+      if (!matchProxyConfig.test(config)) {
+        throw new Error("Config is not the correct shape");
+      }
       config.users.push({
         name: serviceName,
         "allowed-calls": [],
         password: times(() => randomItemString(fullChars), 22).join(""),
       });
+      return config;
     },
   },
   ...[
@@ -87,7 +93,10 @@ const proxyChecks: Array<Check> = [
         if (!matchProxyConfig.test(config)) {
           return "Config is not the correct shape";
         }
-        if (config.users.find((x) => x.name === serviceName)?.["allowed-calls"]?.some((x) => x === operator) ?? false) {
+        if (
+          config.users.find((x) => x.name === serviceName)?.["allowed-calls"]
+            ?.some((x) => x === operator) ?? false
+        ) {
           return;
         }
         return `RPC user "mempool" must have "${operator}" enabled`;
@@ -101,57 +110,65 @@ const proxyChecks: Array<Check> = [
           throw new Error("Users for mempool should exist");
         }
         found["allowed-calls"] = [...(found["allowed-calls"] ?? []), operator];
+        return config;
       },
-    })
+    }),
   ),
 ];
 
-export const dependencies: ExpectedExports.dependencies = {
-    bitcoind: {
-        async check(effects, configInput) {
-            effects.info("check bitcoind");
-            const config = matchBitcoindConfig.unsafeCast(configInput);
-            if (!config.rpc.enable) {
-                return { error: 'Must have RPC enabled' };
-            }
-            if (!config.txindex) {
-                return { error: 'Must have transaction indexing enabled' };
-            }
-            if (config.advanced.pruning.mode=="enabled") {
-                return { error: 'Pruning must be disabled to use Bitcoin Core directly. To use with a pruned node, set Bitcoin Core to Bitcoin Proxy instead.' };
-            }
-            return { result: null };
-        },
-        async autoConfigure(effects, configInput) {
-            effects.info("autoconfigure bitcoind");
-            const config = matchBitcoindConfig.unsafeCast(configInput);
-            config.rpc.enable = true;
-            config.txindex = true;
-            config.advanced.pruning.mode = "disabled";
-            return { result: config };
-        },
+export const dependencies: T.ExpectedExports.dependencies = {
+  bitcoind: {
+    // deno-lint-ignore require-await
+    async check(effects, configInput) {
+      effects.info("check bitcoind");
+      const config = matchBitcoindConfig.unsafeCast(configInput);
+      if (!config.rpc.enable) {
+        return { error: "Must have RPC enabled" };
+      }
+      if (!config.txindex) {
+        return { error: "Must have transaction indexing enabled" };
+      }
+      if (config.advanced.pruning.mode == "enabled") {
+        return {
+          error:
+            "Pruning must be disabled to use Bitcoin Core directly. To use with a pruned node, set Bitcoin Core to Bitcoin Proxy instead.",
+        };
+      }
+      return { result: null };
     },
-    "btc-rpc-proxy": {
-        async check(effects, configInput) {
-            effects.info("check btc-rpc-proxy");
-            for (const checker of proxyChecks) {
-              const error = checker.currentError(configInput);
-              if (error) {
-                effects.error(`throwing error: ${error}`);
-                return { error };
-              }
-            }
-            return { result: null };
-        },
-        async autoConfigure(effects, configInput) {
-          effects.info("autoconfigure btc-rpc-proxy");
-          for (const checker of proxyChecks) {
-            const error = checker.currentError(configInput);
-            if (error) {
-              checker.fix(configInput);
-            }
-          }
-          return { result: configInput };
-        },
+    // deno-lint-ignore require-await
+    async autoConfigure(effects, configInput) {
+      effects.info("autoconfigure bitcoind");
+      const config = matchBitcoindConfig.unsafeCast(configInput);
+      config.rpc.enable = true;
+      config.txindex = true;
+      config.advanced.pruning.mode = "disabled";
+      return { result: config };
     },
+  },
+  "btc-rpc-proxy": {
+    // deno-lint-ignore require-await
+    async check(effects, configInput) {
+      effects.info("check btc-rpc-proxy");
+      for (const checker of proxyChecks) {
+        const error = checker.currentError(configInput);
+        if (error) {
+          effects.error(`throwing error: ${error}`);
+          return { error };
+        }
+      }
+      return { result: null };
+    },
+    // deno-lint-ignore require-await
+    async autoConfigure(effects, configInput) {
+      effects.info("autoconfigure btc-rpc-proxy");
+      for (const checker of proxyChecks) {
+        const error = checker.currentError(configInput);
+        if (error) {
+          configInput = checker.fix(configInput);
+        }
+      }
+      return { result: configInput };
+    },
+  },
 };
