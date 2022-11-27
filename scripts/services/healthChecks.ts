9379d00548647058ed014aa38049f80a309052db
islandbitcoin <dread@start9.com>
2022-11-27 13:56:03 -0500

    update to x86_64 architecture

--- scripts/services/healthChecks.ts
+++ scripts/services/healthChecks.ts
@@ -1,5 +1,8 @@
-import { types as T, healthUtil } from "../deps.ts";
+import { types as T, checkWebUrl, catchError } from "../deps.ts";
 
 export const health: T.ExpectedExports.health = {
-  "web-ui": healthUtil.checkWebUrl("http://mempool.embassy:8080")
-}
+  // deno-lint-ignore require-await
+  async "web-ui"(effects, duration) {
+    return checkWebUrl("http://mempool.embassy:8080")(effects, duration).catch(catchError(effects))
+  },
+};
