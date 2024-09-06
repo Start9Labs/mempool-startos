5adba48388ce70644b9c42f331a5766ca7bd634b
Lucy <12953208+elvece@users.noreply.github.com>
2024-09-06 09:56:34 -0400

    Integration/3.0.0.2 (#34)
    * automatically update pools
    
    * persist system memory size and autoconfig bitcoin maxmempool accordingly
    
    * bump versions and update release notes
    
    * cleanup

--- scripts/procedures/dependencies.ts
+++ scripts/procedures/dependencies.ts
@@ -1,18 +1,34 @@
 import { matches, types as T } from "../deps.ts";
 
-const { shape, string, boolean } = matches;
+const { shape, string, boolean, number } = matches;
 
 const matchBitcoindConfig = shape({
   advanced: shape({
     pruning: shape({
       mode: string,
     }),
+    mempool: shape({
+      maxmempool: number
+    })
   }),
   rpc: shape({
     enable: boolean,
   }),
   txindex: boolean,
 });
+
+async function getSystemMemoryLimit(effects: T.Effects) {
+  try {
+    const memdata = await effects.readFile({ volumeId: 'main', path: 'start9/system_mem_info'})
+    // convert kb to mb
+    const memMB = parseInt(memdata) / 1000
+    return Math.round(memMB / 6)
+  } catch (e) {
+    effects.info(e)
+    // default recommended is 300MB
+    return 300
+  }
+}
 
 export const dependencies: T.ExpectedExports.dependencies = {
   bitcoind: {
@@ -32,6 +48,12 @@ export const dependencies: T.ExpectedExports.dependencies = {
             "Pruning must be disabled to use Bitcoin Core.",
         };
       }
+      const limit = await getSystemMemoryLimit(effects)
+      if (limit > 300 && config.advanced.mempool.maxmempool >= limit) {
+        return {
+          error: "In order to safely run Mempool, Bitcoin Core's \"maxmempool\" size cannot exceed 1/6 of the system RAM"
+        }
+      }
       return { result: null };
     },
     // deno-lint-ignore require-await
@@ -41,6 +63,10 @@ export const dependencies: T.ExpectedExports.dependencies = {
       config.rpc.enable = true;
       config.txindex = true;
       config.advanced.pruning.mode = "disabled";
+      const limit = await getSystemMemoryLimit(effects)
+      if (limit > 300 && config.advanced.mempool.maxmempool >= limit) {
+        config.advanced.mempool.maxmempool = limit
+      }
       return { result: config };
     },
   }
