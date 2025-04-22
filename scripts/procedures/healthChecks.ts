3d8ac3c219c91bac793b503d185795fd33c2eb5a
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2025-04-22 12:16:06 -0400

    increase health check duration

--- scripts/procedures/healthChecks.ts
+++ scripts/procedures/healthChecks.ts
@@ -2,7 +2,9 @@ import { types as T, checkWebUrl, catchError } from "../deps.ts";
 
 export const health: T.ExpectedExports.health = {
   // deno-lint-ignore require-await
-  async "web-ui"(effects, duration) {
-    return checkWebUrl("http://mempool.embassy:8080")(effects, duration).catch(catchError(effects))
+  async "web-ui"(effects, _duration) {
+    return checkWebUrl("http://mempool.embassy:8080")(effects, 30000).catch(
+      catchError(effects)
+    );
   },
 };
