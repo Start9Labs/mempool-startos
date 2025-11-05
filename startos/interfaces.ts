06e2525bf84342d61a72b8bab7a9738b7e2c7732
Shadowy Super Coder <musashidisciple@proton.me>
2025-11-05 15:17:35 -0700

    make multihost backwards compatible

--- startos/interfaces.ts
+++ startos/interfaces.ts
@@ -2,7 +2,7 @@ import { sdk } from './sdk'
 import { uiPort } from './utils'
 
 export const setInterfaces = sdk.setupInterfaces(async ({ effects }) => {
-  const uiMulti = sdk.MultiHost.of(effects, 'multi')
+  const uiMulti = sdk.MultiHost.of(effects, 'main')
   const uiMultiOrigin = await uiMulti.bindPort(uiPort, { protocol: 'http' })
   const ui = sdk.createInterface(effects, {
     name: 'Web UI',
