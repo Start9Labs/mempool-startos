f35d76fa16fe4d7d3a0dfe5f40636f2d9fd7d64e
Matt Hill <mattnine@protonmail.com>
2025-12-21 18:45:14 -0700

    cleaner migration

--- startos/install/versions/v3.2.1_4.ts
+++ startos/install/versions/v3.2.1_4.ts
@@ -1,6 +1,5 @@
-import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
+import { VersionInfo, IMPOSSIBLE, YAML } from '@start9labs/start-sdk'
 import { readFile, rm } from 'fs/promises'
-import { load } from 'js-yaml'
 import { configJson } from '../../file-models/mempool-config.json'
 import { configJsonDefaults } from '../../utils'
 
@@ -10,42 +9,47 @@ export const v_3_2_1_4 = VersionInfo.of({
   migrations: {
     up: async ({ effects }) => {
       const custom = {} as any
-      try {
-        const configYaml = await readFile(
-          '/media/startos/volumes/main/start9/config.yaml',
-          'utf-8',
-        )
-        const config = load(configYaml) as {
-          'enable-electrs'?: boolean
-          indexer?: {
-            type: 'electrs' | 'fulcrum' | 'none'
+      // get old config.yaml
+      const configYaml:
+        | {
+            'enable-electrs'?: boolean
+            indexer?: {
+              type: 'electrs' | 'fulcrum' | 'none'
+            }
+            lightning: {
+              type: 'cln' | 'lnd' | 'none'
+            }
           }
-          lightning: {
-            type: 'cln' | 'lnd' | 'none'
-          }
-        }
-        if (config.lightning.type !== 'none') {
+        | undefined = await readFile(
+        '/media/startos/volumes/main/start9/config.yaml',
+        'utf-8',
+      ).then(YAML.parse, () => undefined)
+
+      if (configYaml) {
+        const { lightning, indexer } = configYaml
+
+        if (lightning.type !== 'none') {
           custom.LIGHTNING.ENABLED = true
-          custom.LIGHTNING.BACKEND = config.lightning.type
+          custom.LIGHTNING.BACKEND = lightning.type
         } else {
           custom.LIGHTNING.ENABLED = false
         }
 
-        if (config['enable-electrs']) {
+        if (configYaml['enable-electrs']) {
           custom.MEMPOOL.BACKEND = 'electrum'
           custom.ELECTRUM = {
             HOST: 'electrs.startos',
             PORT: 50001,
             TLS_ENABLED: false,
           }
-        } else if (config.indexer && config.indexer.type === 'electrs') {
+        } else if (indexer && indexer.type === 'electrs') {
           custom.MEMPOOL.BACKEND = 'electrum'
           custom.ELECTRUM = {
             HOST: 'electrs.startos',
             PORT: 50001,
             TLS_ENABLED: false,
           }
-        } else if (config.indexer && config.indexer.type === 'fulcrum') {
+        } else if (indexer && indexer.type === 'fulcrum') {
           custom.MEMPOOL.BACKEND = 'electrum'
           custom.ELECTRUM = {
             HOST: 'fulcrum.startos',
@@ -54,14 +58,13 @@ export const v_3_2_1_4 = VersionInfo.of({
           }
         }
 
+        // write config defaults
+        await configJson.write(effects, { ...configJsonDefaults, ...custom })
+
         // remove old start9 dir
         rm('/media/startos/volumes/main/start9', {
           recursive: true,
         }).catch(console.error)
-        // write config defaults
-        await configJson.write(effects, { ...configJsonDefaults, ...custom })
-      } catch (error) {
-        console.log('No config.yaml found')
       }
     },
     down: IMPOSSIBLE,
